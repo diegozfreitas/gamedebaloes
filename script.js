c130cafff23e51f289667df4666f73ca8392512a
Diego Z Freitas <ddzadravec@gmail.com>
2021-06-09 13:38:46 -0300

    Atribuindo contador de baloes

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 var total = 0;
+var interval = setInterval(createBalloon, 300)
+
+function stopCreateBalloon() {
+  clearInterval(interval);
+}
+
+function gameOver() {
+  let allBalloons = document.querySelectorAll('div.balloon')
+
+  if (allBalloons.length === 10) {
+    allBalloons.forEach((item, i) => {
+      document.body.removeChild(item);
+    })
+
+    alert("Game Over");
+    stopCreateBalloon();
+    total = 0;
+
+    var r = confirm("Deseja reiniciar o jogo!");
+    if (r == true) {
+      window.location.reload()
+    } else {
+      stopCreateBalloon()
+    }
+  }
+}
 
 function createBalloon() {
   var balloon = document.createElement('div');
@@ -11,15 +37,43 @@ function createBalloon() {
   balloon.setAttribute('onClick', 'explodeBalloon(this)');
 
   document.body.appendChild(balloon);
+
+  gameOver()
 }
 
 function explodeBalloon(obj) {
   document.body.removeChild(obj);
+
   total++;
+
   var score = document.getElementById('total');
-  score.innerHTML = `Balões estourados ${total}`
+  score.innerHTML = ` ${total}`
 }
 
+function score() {
+  let allBalloons = document.querySelectorAll('div.balloon')
+
+  var numberOfBalloons = document.getElementById('numberOfBalloons');
+  numberOfBalloons.innerHTML = ` ${allBalloons.length}`
+
+  console.log('eee', allBalloons.length)
+
+  if (allBalloons.length > 80) {
+    numberOfBalloons.setAttribute('style', 'color: red;')
+  } else if (allBalloons.length > 50) {
+    numberOfBalloons.setAttribute('style', 'color: orange;')
+  } else {
+    numberOfBalloons.setAttribute('style', 'color: black;')
+  }
+}
+
+
 function loadGame() {
-  setInterval(createBalloon, 300)
+  var r = confirm("Deseja iniciar o jogo!");
+  if (r == true) {
+    interval
+    setInterval(score, 50);
+  } else {
+    stopCreateBalloon()
+  }
 }
